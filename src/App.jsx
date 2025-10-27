6678d04b21455e79f00bc5655874e46315203bd6
krestwastaken <martynskelvin@gmail.com>
2025-10-27 21:56:58 +0100

    final?

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,66 @@
+import { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      if (newIndex > people.length - 1) {
+        return 0;
+      }
+      return newIndex;
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      if (newIndex < 0) {
+        return people.length - 1;
+      }
+      return newIndex;
+    });
+  };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length)
+    if(randomNumber === index){
+      randomNumber = index + 1
+    }
+    // let newRandomNumber = randomNumber
+    // if (newRandomNumber )
+    setIndex(randomNumber)
+  }
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn-hipster btn" onClick={randomPerson}>
+          Surprise me
+        </button>
+      </article>
+    </main>
+  );
 };
 export default App;
